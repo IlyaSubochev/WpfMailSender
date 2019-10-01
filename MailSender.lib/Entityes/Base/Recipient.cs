f41a600572bc98f92dcb6221be731e906a338caf
Subochev Ilya <siveternal@gmail.com>
2019-10-01 20:04:48 +0300

    Error message

--- MailSender.lib/Entityes/Base/Recipient.cs
+++ MailSender.lib/Entityes/Base/Recipient.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MailSender.lib.Entityes.Base
 {
-    public class Recipient : HumanEntity 
+    public class Recipient : HumanEntity, IDataErrorInfo
     
     {
+      
         public override string Name
         {
             get => base.Name;
@@ -16,6 +18,28 @@ namespace MailSender.lib.Entityes.Base
             {
                 base.Name = value;
             }
+        }
+
+        string IDataErrorInfo.Error => "";
+
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
+            {
+                switch (PropertyName)
+                {
+                    default: return string.Empty;
+
+                    case nameof(Name):
+                        var name = Name;
+                        if (name is null) return "Отсутсвует ссылка на строку с именем";
+                        if (name.Length < 4) return "Имя должно быть длиннее 3-х символов";
+                        if (!char.IsLetter(name[0])) return "Имя должно начинаться с буквы";
+                        return string.Empty;
+
+                }
+            }
+
         }
     }
 }
